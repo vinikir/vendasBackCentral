3e0d37d7061d329f9e4da4a8db836652906bff65
VinIKir <vinikir@yahoo.com.br>
2024-10-11 13:35:21 -0300

    ajustes

---
+++ src/enums/CategoriaProdutosEnums.ts
@@ -0,0 +1,12 @@
+export enum CategoriaProdutosEnums{
+    Motor = "motor",
+    Suspencao = "suspencao",
+    Roda = "roda",
+    Carenagem = "carenagem",
+    Acessorio = "acessorio",
+    Eletronico = "eletronico",
+    Cabo = "cabo",
+    Eletrica = "eletrica",
+    Penu = "penu",
+    Freio = "freios"
+}
